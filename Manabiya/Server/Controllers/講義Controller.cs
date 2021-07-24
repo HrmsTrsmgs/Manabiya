5d68b2ae01b1d59c8a5105a0e987787ea77023b1
新田光宏 <potimarimo@gmail.com>
2021-07-25 03:54:25 +0900

    講義Controllerの追加

---
+++ Manabiya/Server/Controllers/講義Controller.cs
@@ -0,0 +1,51 @@
+﻿using Marimo.Manabiya.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Marimo.Manabiya.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class 講義Controller : ControllerBase
+    {
+        static Dictionary<int, 講義> データ { get; } = new Dictionary<int, 講義>();
+
+        // GET: <講義Controller>
+        [HttpGet]
+        public IEnumerable<講義> Get() => データ.Values;
+
+        // GET <講義Controller>/5
+        [HttpGet("{id}")]
+        public 講義 Get(int id) =>
+            データ.ContainsKey(id)
+            ? データ[id]
+            : null;
+
+
+        // POST <講義Controller>
+        [HttpPost]
+        public void Post([FromBody] 講義 value)
+        {
+            Put(value);
+        }
+
+        // PUT <講義Controller>
+        [HttpPut]
+        public void Put([FromBody] 講義 value)
+        {
+            データ[value.Id] = value;
+        }
+
+        // DELETE <講義Controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            データ.Remove(id);
+        }
+    }
+}
---
+++ Manabiya/Shared/講義.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.Manabiya.Shared
+{
+    public class 講義
+    {
+        public int Id { get; init; }
+        public string タイトル { get; set; }
+    }
+}
