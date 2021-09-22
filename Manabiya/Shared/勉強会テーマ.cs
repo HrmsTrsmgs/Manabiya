3ecd28d6a740e9e7a8829f3ea3ec4f6ef5977113
NITTA Mitsuhiro <potimarimo@gmail.com>
2021-09-22 20:05:21 +0900

    入力formの作成

--- Manabiya/Shared/勉強会テーマ.cs
+++ Manabiya/Shared/勉強会テーマ.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Marimo.Manabiya.Shared
     public class 勉強会テーマ
     {
         public int Id { get; init; }
+
+        [Required(ErrorMessage ="タイトルを入力してください。")]
         public string タイトル { get; set; }
     }
 }
